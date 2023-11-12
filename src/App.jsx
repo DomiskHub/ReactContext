47074fe624fd82b9bce0c4fa719d7ce9b5913a83
Dominque Piffardi <domiskp@gmail.com>
2023-11-12 00:36:58 -0300

    not-found added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Favorites from "./views/Favorites";
 import Home from "./views/Home";
 import GlobalProvider from "./context/GlobalContext";
+import Notfound from "./views/NotFound";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/favoritos" element={<Favorites />} />
+          <Route path="*" element={<Notfound />} />
         </Routes>
       </BrowserRouter>
     </GlobalProvider>
